339cdf8501840e7839f6b988860fffcae93e8700
WeBber <bird23074035@gmail.com>
2018-02-27 14:16:01 +0800

    feat(update):

--- reactWeb/src/App.js
+++ reactWeb/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={'./'+logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hey I'm Wbbbbb</h1>
         </header>
         <p className="App-intro">
